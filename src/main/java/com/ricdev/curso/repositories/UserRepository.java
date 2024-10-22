3dc80ed708d71e6f0dca020c5fe538a13e5e09b7
Ricardo-Alves-Dev <rpsevla@gmail.com>
2024-10-22 13:26:51 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/ricdev/curso/repositories/UserRepository.java
+++ src/main/java/com/ricdev/curso/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.ricdev.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ricdev.curso.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
